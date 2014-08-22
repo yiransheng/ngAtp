391a186f6d65014948cfd0f24f8537cf132064ca
Yiran Sheng <shengyiran@gmail.com>
2014-08-22 17:57:39 -0400

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,38 @@
 module.exports = function(grunt) {
-	var banner = [
-    '//     <%= pkg.name %> <%= pkg.version %>',
-    '//     <%= pkg.homepage %>',
-    '//     (c) 2014 <%= pkg.author %>',
-    '//     <%= pkg.name %> may be freely distributed under the MIT license.'
-  ].join('\n');
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    rig : {
-      compile: {
-			  options: {
-				  banner: banner + '\n\n'
-		    },
-		    files: {
-				  'dist/js/<%= pkg.name %>-bundle.js': [
-		        'src/ngAtp.js'
-		      ]
-		    }
-	    }
+    var banner = [
+        '//     <%= pkg.name %> <%= pkg.version %>',
+        '//     <%= pkg.homepage %>',
+        '//     (c) 2014 <%= pkg.author %>',
+        '//     <%= pkg.name %> may be freely distributed under the MIT license.'
+    ].join('\n');
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        rig: {
+            compile: {
+                options: {
+                    banner: banner + '\n\n'
+                },
+                files: {
+                    'dist/js/<%= pkg.name %>-bundle.js': [
+                        'src/ngAtp.js'
+                    ]
+                }
+            }
 
-    },
-		uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'dist/js/<%= pkg.name %>-bundle.min.js': ['dist/js/<%= pkg.name %>-bundle.js']
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/js/<%= pkg.name %>-bundle.min.js': ['dist/js/<%= pkg.name %>-bundle.js']
+                }
+            }
         }
-      }
-    }
-  });
+    });
 
-	grunt.loadNpmTasks('grunt-rigger');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['rig', 'uglify']);
+    grunt.loadNpmTasks('grunt-rigger');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.registerTask('default', ['rig', 'uglify']);
 }
-
